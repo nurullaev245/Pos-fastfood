12019fefd12598aeacdb25affa74f0c018244080
Mirodiluser <hayitovmirodil12@gamil.com>
2024-07-29 13:41:14 +0500

    DD

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,35 +1,52 @@
 import React, { useState } from 'react';
-import { useOutletContext } from 'react-router-dom';
 
 const Signin = () => {
-    const [userData, setUserData] = useState({ email: '', password: '' });
-    const { handleLogin } = useOutletContext();
+  const [userData, setuserData] = useState({
+    email: '',
+    password: ''
+  });
 
-    const handleChange = (e) => {
-        setUserData({ ...userData, [e.target.name]: e.target.value });
-    };
+  const [responseUser, setresponseUser] = useState(null);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (userData.email !== "" && userData.password !== "") {
-            handleLogin(userData.email, userData.password);
-        } else {
-            alert('Заполните все поля');
-        }
-    };
+  const handleChange = (e) => {
+    setuserData({ ...userData, [e.target.name]: e.target.value });
+    console.log(e.target.value);
+  };
 
-    return (
-        <div className=''>
-            <form className='bg-base-300 py-20 px-10 max-w-[50%] rounded-2xl mx-auto my-10 flex flex-col gap-4' onSubmit={handleSubmit}>
-                <p className='text-center font-bold tracking-wider text-4xl'>Авторизация</p>
-                <div className='flex flex-col gap-4'>
-                    <input type='text' placeholder='Введите почту' className='py-2 px-4 rounded-lg' name='email' onChange={handleChange} />
-                    <input type='password' placeholder='Введите пароль' className='py-2 px-4 rounded-lg' name='password' onChange={handleChange} />
-                    <button className='btn btn-primary'>Войти</button>
-                </div>
-            </form>
+  const requestData = async () => {
+    try {
+      let request = await fetch('http://localhost:3000/users');
+      let response = await request.json();
+      let find = response.find(user => user.email === userData.email && user.password === userData.password);
+      return setresponseUser(find);
+    } catch (err) {
+      console.log("Error: ", err);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (userData.email !== "" && userData.password !== "") {
+      let res = requestData();
+      console.log("RES", res);
+      console.log("Togri");
+    } else {
+      console.log("notogri");
+    }
+  };
+
+  return (
+    <div className=''>
+      <form className='bg-base-300 py-20 px-10 max-w-[50%] rounded-2xl mx-auto my-10 flex flex-col gap-4' onSubmit={handleSubmit}>
+        <p className='text-center font-bold tracking-wider text-4xl'>Авторизация</p>
+        <div className='flex flex-col gap-4'>
+          <input type='text' placeholder='Введите почту' className='py-2 px-4 rounded-lg' name='email' onChange={handleChange} />
+          <input type='text' placeholder='Введите пароль' className='py-2 px-4 rounded-lg' name='password' onChange={handleChange} />
+          <button className='btn btn-primary'>Войти</button>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default Signin;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,40 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Signup = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+  const [regisetred, setRegistred] = useState("");
+  const [adress, setAdress] = useState({
+    street: "User Street",
+    city: "User City",
+    state: "User State",
+    country: "User Country",
+  });
+  const [roles, setRoles] = useState("");
+  const [status, setStatus] = useState("");
+  const [image, setImage] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
-  const handleSubmit = async(event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     if (!username) {
-      alert('Придумайте логин!');
+      alert("Придумайте логин!");
       return;
     }
     if (!password) {
-      alert('Придумайте пароль!');
+      alert("Придумайте пароль!");
       return;
     }
     if (!email) {
-      alert('Напишите email!');
+      alert("Напишите email!");
       return;
     }
     if (!phone) {
-      alert('Напишите номер телефона!');
+      alert("Напишите номер телефона!");
       return;
     }
 
     if (isSubmitted) {
       try {
-        const response = await fetch('https://localhost:3000/users', {
-          method: 'POST',
+        const response = await fetch("https://localhost:3000/users", {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify({
             username,
+            age,
+            birthdate,
+            regisetered,
+            adress,
+            roles,
+            status,
+            image,
             password,
             email,
             phone,
@@ -42,61 +61,139 @@ const Signup = () => {
         });
 
         if (response.ok) {
-          alert('Регистрация успешно прошла')
+          alert("Регистрация успешно прошла");
           setIsSubmitted(true);
         } else {
-          alert('Ошибка регистрации!')
+          alert("Ошибка регистрации!");
         }
       } catch (e) {
-        alert('Ошибка регистарции!')
+        alert("Ошибка регистарции!");
       }
     }
     setIsSubmitted(true);
   };
 
   if (isSubmitted) {
-    return <div className='text-center font-bold text-4xl py-40 px-10'>Регистрация успешно прошла!</div>;
+    return (
+      <div className="text-center font-bold text-4xl py-40 px-10">
+        Регистрация успешно прошла!
+      </div>
+    );
   }
 
   return (
     <div>
-      <form 
-        onSubmit={handleSubmit} 
-        className='bg-base-300 py-20 px-10 max-w-[50%] rounded-2xl mx-auto my-10 flex flex-col gap-4'
+      <form
+        onSubmit={handleSubmit}
+        className="bg-base-300 py-20 px-10 max-w-[50%] rounded-2xl mx-auto my-10 flex flex-col gap-4"
       >
-        <p className='text-center font-bold tracking-wider text-4xl'>Регистрация</p>
-        <div className='flex flex-col gap-4'>
-          <input 
-            type='text' 
-            placeholder='Придумайте логин' 
-            className='py-2 px-4 rounded-lg' 
-            value={username} 
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input 
-            type='password' 
-            placeholder='Придумайте пароль' 
-            className='py-2 px-4 rounded-lg' 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <input 
-            type='email' 
-            placeholder='Введите @email' 
-            className='py-2 px-4 rounded-lg' 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input 
-            type='tel' 
-            placeholder='Введите тел.номер' 
-            className='py-2 px-4 rounded-lg' 
-            value={phone} 
-            onChange={(e) => setPhone(e.target.value)}
-          />
-          <button type='submit' className='btn btn-primary'>Готово</button>
+        <p className="text-center font-bold tracking-wider text-4xl">
+          Регистрация
+        </p>
+        <div className="flex flex-col gap-4">
+          <label className="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
+              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+            </svg>
+            <input
+              type="text"
+              placeholder="Придумайте логин"
+              className="grow"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <input
+              type="password"
+              placeholder="Придумайте пароль"
+              className="grow"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <input
+              type="date"
+              placeholder="Введите дату рождения"
+              className="grow"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
+              <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+              <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+            </svg>
+            <input
+              type="email"
+              placeholder="Введите @email"
+              className="grow"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <input
+              type="tel"
+              placeholder="Введите тел.номер"
+              className="grow"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </label>
+          <button type="submit" className="btn btn-primary">
+            Готово
+          </button>
         </div>
-      </form> 
+      </form>
     </div>
   );
 };
